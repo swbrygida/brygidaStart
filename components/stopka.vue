5610af6130c26c83bc1ac5c80ad80e384455ff92
lukeandgladys <lukeandgladys@gmail.com>
2023-11-28 13:28:02 +0100

    footer

---
+++ components/stopka.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+      <div class="stopka">
+        <p>
+          <NuxtLink  target="_blank"  to="https://pobrygidkowski.pl/pl/aktualno%C5%9Bci/548-stworzenie-nowoczesnej-aplikacji-internetowej-oraz-udost%C4%99pnienie-przewodnika-wirtualnego-3d-online-2.html"
+            >Dofinansowano ze środków Ministra Kultury i Dziedzictwa Narodowego pochodzących z Funduszu Promocji Kultury – państwowego funduszu celowego </NuxtLink
+          >
+        </p>
+      </div>
+    </main>
+  </template>
+  <style>
+  .stopka {
+    width: 90vw;
+    height: auto;
+    padding: 1em 5vw;
+    border-top: 2px solid #f2f2f2;
+    text-align: center;
+    font-size: large;
+    font-weight: 800;
+  }
+  </style>
+  
