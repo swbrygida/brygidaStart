6e0eaf6bd151162b8deaba9ecd6c2c626d94bf4b
lukeandgladys <lukeandgladys@gmail.com>
2023-11-30 09:58:17 +0100

    info

--- components/stopka.vue
+++ components/stopka.vue
@@ -1,23 +1,28 @@
 <template>
-    <main>
-      <div class="stopka">
-        <p>
-          <NuxtLink  target="_blank"  to="https://pobrygidkowski.pl/pl/aktualno%C5%9Bci/548-stworzenie-nowoczesnej-aplikacji-internetowej-oraz-udost%C4%99pnienie-przewodnika-wirtualnego-3d-online-2.html"
-            >Dofinansowano ze środków Ministra Kultury i Dziedzictwa Narodowego pochodzących z Funduszu Promocji Kultury – państwowego funduszu celowego </NuxtLink
-          >
-        </p>
-      </div>
-    </main>
-  </template>
-  <style>
-  .stopka {
-    width: 90vw;
-    height: auto;
-    padding: 1em 5vw;
-    border-top: 2px solid #f2f2f2;
-    text-align: center;
-    font-size: large;
-    font-weight: 800;
-  }
-  </style>
-  
+  <main>
+    <div class="stopka">
+      <p>
+        <NuxtLink  target="_blank"  to="/info"
+          >Dofinansowano ze środków MKiDN</NuxtLink
+        >
+      </p>
+    </div>
+  </main>
+</template>
+<style>
+.stopka {
+  width: 90vw;
+  height: auto;
+  padding: 1em 5vw;
+  border-top: 2px solid #f2f2f2;
+  background-color: #fff;
+  text-align: center;
+  font-size: large;
+  font-weight: 800;
+  position: fixed;
+  bottom: 0; left: 0;
+  z-index: 5000000000;
+
+}
+
+</style>
---
+++ pages/info.vue
@@ -0,0 +1,53 @@
+<template>
+    <main>
+    <Belka />
+  
+    <img src="../icons/info.jpg" class="infoo" alt="Informacja o dopinansowaniu projektu ze środków Ministerstwa Kultury i 
+    Dziedzictwa Narodowego">
+    <div class="dofinansowano">
+    <p>
+     Projekt zakłada zbudowanie aplikacji internetowej, która w sposób przyjazny użytkownikom, przy użyciu współczesnych technologii internetowych, będzie udostępniała, prezentowała i pomagała wyszukać, wszystkie dotychczas zdigitalizowane obiekty związane z kościołem pobrygidkowskim. 
+ 
+ Będą to w szczególności - eksponaty i zabytkowe wyposażenie, gigapanoramy ołtarzy, makiety przebudowy kościoła, panorama lotnicza, panorama pól grunwaldzkich, linia czasu św. Brygidy, tabliczki memoratywne, obrazy emblematyczne, świątynia 3d - film i spacer oraz stworzony wirtualny przewodnik 3d po kościele.
+ 
+ Na potrzeby projektu wykorzystane zostaną zdigitalizowane w ramach działania - Kultura Cyfrowa 2016, 2017 oraz 2021 – zasoby, które stworzą spójną, nowoczesną i dostępną w Internecie ofertę, będącą jednocześnie podsumowaniem dotychczas zrealizowanych projektów.
+ 
+  </p><p>
+ 
+ W związku z realizacją wniosku z Kultury Cyfrowej 2023 wykonano: <br>
+ 
+ 1. Zbudowano aplikację internetową, która w sposób przyjazny użytkownikom, przy użyciu współczesnych technologii internetowych udostępnia, prezentuje i pomaga wyszukać wg metadanych, wszystkie dotychczas zdigitalizowane obiekty związane z kościołem pobrygidkowskim, tworząc podsumowanie dotychczas zrealizowanych projektów w ramach programu Kultura Cyfrowa.
+ <br>
+ Modele zabytków oraz tabliczek memoratywnych dostępne są pod adresem:
+ <br>
+ <NuxtLink to="https://brygida-arch.vercel.app">
+ https://brygida-arch.vercel.app </NuxtLink>
+ <br>
+ 2. Udostępniono przewodnik wirtualny 3d po kościele w formie online oraz zintegrowano przewodnik z aplikacją internetową, by obie części tworzyły spójną całość. Łącznie projekt doprowadził do udostępnienia 318 obiektów.
+ <br>
+ Modele kościoła wewnątrz i zewnątrz dostępne są pod adresem:
+ <br>
+ <NuxtLink to="https://brygida-model.vercel.app/">
+ https://brygida-model.vercel.app/ </NuxtLink>
+ <br>
+ 3. Dodatkowo całość uwzględniające wszystkie zrealizowane dotychczas projekty znajduje się pod adresem:
+ <br>
+ <NuxtLink to="https://brygida-start.vercel.app/ ">
+ https://brygida-start.vercel.app/ </NuxtLink>
+ <br>
+ Zapraszamy!</p>
+ </div>
+    </main>
+   </template>
+   <style>
+   .dofinansowano p {
+     width: 80%;
+     margin: 0 10%;
+   }
+   .dofinansowano {
+     margin: 0 0 4em 0;
+   }
+   img.infoo {
+    width: 100%!important;
+   }
+   </style>
