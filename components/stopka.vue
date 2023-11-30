d51bb6a2c174e54756409962aa98dbd85744f7a5
lukeandgladys <lukeandgladys@gmail.com>
2023-11-30 10:47:34 +0100

    fix infotext

--- components/stopka.vue
+++ components/stopka.vue
@@ -3,25 +3,29 @@
     <div class="stopka">
       <p>
         <NuxtLink  target="_blank"  to="/info"
-          >Dofinansowano ze środków MKiDN</NuxtLink
+          >Dofinansowano ze środków Ministerstwa Kultury i Dziedzictwa Narodowego pochodzących z Funduszu Promocji Kultury - państwowego funduszu celowego</NuxtLink
         >
       </p>
     </div>
   </main>
 </template>
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;800&display=swap");
+
 .stopka {
   width: 90vw;
   height: auto;
-  padding: 1em 5vw;
+  padding: 0 5vw;
   border-top: 2px solid #f2f2f2;
   background-color: #fff;
   text-align: center;
-  font-size: large;
+  font-size: normal;
   font-weight: 800;
   position: fixed;
   bottom: 0; left: 0;
-  z-index: 5000000000;
+  font-family: "Inknut Antiqua", serif;
+  line-height: 1.8rem
+  
 
 }
 
